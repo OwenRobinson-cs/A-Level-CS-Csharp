99bed531ef2cf783e1452103244b56c39ec45b70
owen robinson <ORR-DesktopHome>
2020-10-21 22:22:45 +0100

    Complete Program

--- CheckDigitForms/CheckDigitForms/Form1.Designer.cs
+++ CheckDigitForms/CheckDigitForms/Form1.Designer.cs
@@ -32,14 +32,16 @@
             this.LBL_Output = new System.Windows.Forms.Label();
             this.TXT_Input = new System.Windows.Forms.TextBox();
             this.LBL_History = new System.Windows.Forms.Label();
+            this.BTN_GenerateCheckDigit = new System.Windows.Forms.Button();
+            this.BTN_Clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BTN_Submit
             // 
-            this.BTN_Submit.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_Submit.Location = new System.Drawing.Point(197, 226);
+            this.BTN_Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Submit.Location = new System.Drawing.Point(197, 213);
             this.BTN_Submit.Name = "BTN_Submit";
-            this.BTN_Submit.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Submit.Size = new System.Drawing.Size(75, 36);
             this.BTN_Submit.TabIndex = 0;
             this.BTN_Submit.Text = "Submit";
             this.BTN_Submit.UseVisualStyleBackColor = true;
@@ -48,10 +50,10 @@
             // LBL_Output
             // 
             this.LBL_Output.AutoSize = true;
-            this.LBL_Output.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LBL_Output.Location = new System.Drawing.Point(13, 232);
             this.LBL_Output.Name = "LBL_Output";
-            this.LBL_Output.Size = new System.Drawing.Size(68, 17);
+            this.LBL_Output.Size = new System.Drawing.Size(58, 20);
             this.LBL_Output.TabIndex = 1;
             this.LBL_Output.Text = "Output";
             // 
@@ -70,11 +72,34 @@
             this.LBL_History.Size = new System.Drawing.Size(0, 13);
             this.LBL_History.TabIndex = 3;
             // 
+            // BTN_GenerateCheckDigit
+            // 
+            this.BTN_GenerateCheckDigit.Location = new System.Drawing.Point(197, 171);
+            this.BTN_GenerateCheckDigit.Name = "BTN_GenerateCheckDigit";
+            this.BTN_GenerateCheckDigit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BTN_GenerateCheckDigit.Size = new System.Drawing.Size(75, 36);
+            this.BTN_GenerateCheckDigit.TabIndex = 4;
+            this.BTN_GenerateCheckDigit.Text = "Generate Check Digit";
+            this.BTN_GenerateCheckDigit.UseVisualStyleBackColor = true;
+            this.BTN_GenerateCheckDigit.Click += new System.EventHandler(this.BTN_GenerateCheckDigit_Click);
+            // 
+            // BTN_Clear
+            // 
+            this.BTN_Clear.Location = new System.Drawing.Point(197, 3);
+            this.BTN_Clear.Name = "BTN_Clear";
+            this.BTN_Clear.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Clear.TabIndex = 5;
+            this.BTN_Clear.Text = "Clear History";
+            this.BTN_Clear.UseVisualStyleBackColor = true;
+            this.BTN_Clear.Click += new System.EventHandler(this.BTN_Clear_Click);
+            // 
             // CheckDigit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BTN_Clear);
+            this.Controls.Add(this.BTN_GenerateCheckDigit);
             this.Controls.Add(this.LBL_History);
             this.Controls.Add(this.TXT_Input);
             this.Controls.Add(this.LBL_Output);
@@ -92,6 +117,8 @@
         private System.Windows.Forms.Label LBL_Output;
         private System.Windows.Forms.TextBox TXT_Input;
         private System.Windows.Forms.Label LBL_History;
+        private System.Windows.Forms.Button BTN_GenerateCheckDigit;
+        private System.Windows.Forms.Button BTN_Clear;
     }
 }
 
--- CheckDigitForms/CheckDigitForms/Form1.cs
+++ CheckDigitForms/CheckDigitForms/Form1.cs
@@ -15,72 +15,119 @@ namespace CheckDigitForms
         public CheckDigit()
         {
             InitializeComponent();
+            LBL_History.Text = "History: ";
+            
+            LBL_Output.Text = null;
         }
 
         private void BTN_Submit_Click(object sender, EventArgs e)
         {
-            // Define Variables
-            string input;
-            int inputtedCheckSum;
-            int newCheckSum;
-            int lastDigit;
+            try
+            {
+                // Define Variables
+                string input;
+                int inputtedCheckSum;
+                int lastDigit;
+                // Take input
+
+                input = TXT_Input.Text;
+
+                // Remove last digit from input 
+
+                // Find last digit
+                lastDigit = input.Length - 1;
+
+                int.TryParse(input[lastDigit].ToString(), out inputtedCheckSum);
+                input = input.Remove(input.Length - 1, 1);
+
+                // Compare to removed digit
+                Console.Beep();
+                if (CalcualteCheckDigit(input) != inputtedCheckSum)
+                {
+
+                    LBL_Output.Text = "Invalid Check Digit.";
+                }
+                else
+                {
+
+                    LBL_Output.Text = "Valid Check Digit";
+                }
+
+                // Store in history
+
+                LBL_History.Text += "\n " + input + inputtedCheckSum;
+            }
+            catch
+            {
+                Console.Beep();
+                LBL_Output.Text = "ERROR, INVALID INPUT.";
+            }
+        }
+        private void BTN_GenerateCheckDigit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TXT_Input.TextLength < 13)
+                {
+                    LBL_Output.Text = "Check Digit = " + CalcualteCheckDigit(TXT_Input.Text).ToString();
+
+                    // Store in History 
+                    LBL_History.ForeColor = Color.Red;
+                    LBL_History.Text += "\nCalculation: " + TXT_Input.Text + "  " + CalcualteCheckDigit(TXT_Input.Text);
+                    LBL_History.ForeColor = Color.Black;
+                }
+                else
+                {
+                    Console.Beep();
+                    Console.Beep();
+                    LBL_Output.Text = "ERROR, INVALID INPUT.";
+                }
+            }
+            catch
+            {
+                Console.Beep();
+                Console.Beep();
+                LBL_Output.Text = "ERROR, INVALID INPUT.";
+            }
+        }
+        private int CalcualteCheckDigit(string ean)
+        {
+            int[] values = new int[12];
             int sumOfDigits = 0;
-            int remainder;
-            int[] values = new int[13];
-            // Take input
+            int checkDigit;
 
-            input = TXT_Input.Text;
-
-            // Remove last digit from input 
-
-            // Find last digit
-            lastDigit = input.Length - 1;
-
-            int.TryParse(input[lastDigit].ToString(), out inputtedCheckSum);
-            input = input.Remove(input.Length - 1, 1);
-
-            // Multiply as necessary (every other num is *3)
-
-            values[0] = int.Parse(input[0].ToString()) * 3;
-            values[1] = int.Parse(input[1].ToString());
-            values[2] = int.Parse(input[2].ToString()) * 3;
-            values[3] = int.Parse(input[3].ToString());
-            values[4] = int.Parse(input[4].ToString()) * 3;
-            values[5] = int.Parse(input[5].ToString());
-            values[6] = int.Parse(input[6].ToString()) * 3;
-            values[7] = int.Parse(input[7].ToString());
-            values[8] = int.Parse(input[8].ToString()) * 3;
-            values[9] = int.Parse(input[9].ToString());
-            values[10] = int.Parse(input[10].ToString()) * 3;
-            values[11] = int.Parse(input[11].ToString());
-            values[12] = 0 * 3;
+            values[0] = int.Parse(ean[0].ToString());
+            values[1] = int.Parse(ean[1].ToString()) * 3;
+            values[2] = int.Parse(ean[2].ToString());
+            values[3] = int.Parse(ean[3].ToString()) * 3;
+            values[4] = int.Parse(ean[4].ToString());
+            values[5] = int.Parse(ean[5].ToString()) * 3;
+            values[6] = int.Parse(ean[6].ToString());
+            values[7] = int.Parse(ean[7].ToString()) * 3;
+            values[8] = int.Parse(ean[8].ToString());
+            values[9] = int.Parse(ean[9].ToString()) * 3;
+            values[10] = int.Parse(ean[10].ToString());
+            values[11] = int.Parse(ean[11].ToString()) * 3;
 
             for (int i = 0; i < 12; i++)
             {
                 sumOfDigits += values[i];
             }
-            // x mod 10 = r 
-            remainder = sumOfDigits % 10;
-            // 10 - r = checkdigit
-            newCheckSum = 10 - remainder;
 
-            // Compare to removed digit
-            if ( newCheckSum != inputtedCheckSum)
-            {
-                Console.Beep();
-                LBL_Output.Text = "Invalid Check Digit.";
-            }
-            else
-            {
-                LBL_Output.Text = "Valid Check Digit";
-            }
-
-            // Store in history
             
-            LBL_History.Text += "\n " + input; 
+            checkDigit = 10 - (sumOfDigits % 10);
 
+            if (checkDigit >= 10)
+            {
+                checkDigit = 0;
+            }
+
+            return checkDigit;
         }
 
-        
+        private void BTN_Clear_Click(object sender, EventArgs e)
+        {
+            LBL_History.Text = "History:";
+        }
     }
 }
